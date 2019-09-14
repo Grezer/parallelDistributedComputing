5c2d607add59d1703e4b132fda4dbe7fd2cc5a28
Name <rzaevnr@list.ru>
2019-09-14 22:35:43 +0300

    Try create matrix

---
+++ MatrixMultiplication/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MatrixMultiplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create and multiply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Matrix size";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(15, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(24, 44);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(75, 40);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(24, 44);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(152, 40);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(24, 44);
+            this.panel3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(348, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 176);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MatrixMultiplication/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrixMultiplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[,] firstMatrix;
+        int[,] secondMatrix;
+        int[,] resultMatrix;
+        Label[,] firstMatrixLabels;
+        Label[,] secondMatrixLabels;
+        Label[,] resMatrixLabels;
+        int matrixSize;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear(); panel2.Controls.Clear(); panel3.Controls.Clear();
+            Random rnd = new Random();
+            matrixSize = Convert.ToInt32(textBox1.Text);
+            int n = matrixSize * 20 + 5 * (matrixSize - 1);
+            panel1.Size = new Size(n, n); panel2.Size = new Size(n, n); panel3.Size = new Size(n, n);
+            panel2.Location = new Point(n + 40, 40); panel3.Location = new Point(n * 2 + 60, 40);
+            label2.Location = new Point(n + 20, 30 + n / 2); label2.Text = "X";
+            label3.Location = new Point(n * 2 + 45, 30 + n / 2); label3.Text = "=";
+            firstMatrix = new int[matrixSize, matrixSize];
+            secondMatrix = new int[matrixSize, matrixSize];
+            resultMatrix = new int[matrixSize, matrixSize];
+            firstMatrixLabels = new Label[matrixSize, matrixSize];
+            secondMatrixLabels = new Label[matrixSize, matrixSize];
+            resMatrixLabels = new Label[matrixSize, matrixSize];
+
+            for (int i = 0; i < matrixSize; i++)
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    firstMatrix[i, j] = rnd.Next(1, 4);
+                    firstMatrixLabels[i, j] = new Label();
+                    firstMatrixLabels[i, j].Text = firstMatrix[i, j].ToString();
+                    firstMatrixLabels[i, j].Size = new Size(20, 20);
+                    firstMatrixLabels[i, j].Location = new Point(j * 25, i * 25);
+                    panel1.Controls.Add(firstMatrixLabels[i, j]);
+                }
+
+            for (int i = 0; i < matrixSize; i++)
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    secondMatrix[i, j] = rnd.Next(1, 4);
+                    secondMatrixLabels[i, j] = new Label();
+                    secondMatrixLabels[i, j].Text = secondMatrix[i, j].ToString();
+                    secondMatrixLabels[i, j].Size = new Size(20, 20);
+                    secondMatrixLabels[i, j].Location = new Point(j * 25, i * 25);
+                    panel2.Controls.Add(secondMatrixLabels[i, j]);
+
+                }
+
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < matrixSize; i++)            
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    Task<int> task1 = new Task<int>(() => getResult(i, j));
+                    tasks.Add(new Task(delegate {
+                        getResult(i, j);
+                    }));
+                    
+                    //resultMatrix[i, j] = getResult(i, j);
+                    //resMatrixLabels[i, j] = new Label();
+                    //resMatrixLabels[i, j].Text = resultMatrix[i, j].ToString();
+                    //resMatrixLabels[i, j].Size = new Size(20, 20);
+                    //resMatrixLabels[i, j].Location = new Point(j * 25, i * 25);
+                    //panel3.Controls.Add(resMatrixLabels[i, j]);
+                }
+
+            foreach (var i in tasks)
+            {
+                i.Start();
+            }
+        }
+
+        public void getResult(int x, int y)
+        {
+            int res = 0;
+            for (int i = 0; i < matrixSize; i++)           
+                res += firstMatrix[i, y] * secondMatrix[x, i];
+            resultMatrix[x, y] = res;
+            resMatrixLabels[x, y] = new Label();
+            resMatrixLabels[x, y].Text = res.ToString();
+            resMatrixLabels[x, y].Size = new Size(20, 20);
+            resMatrixLabels[x, y].Location = new Point(y * 25, x * 25);
+            panel3.Controls.Add(resMatrixLabels[x, y]);
+        }
+
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = (int.TryParse(textBox1.Text, out var x) ? true : false);
+        }
+    }
+}
