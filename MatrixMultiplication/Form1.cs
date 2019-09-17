b790ce0141e55b8cd81699326a85c260193c3108
Name <rzaevnr@list.ru>
2019-09-17 19:01:59 +0300

    Matrix ready

--- MatrixMultiplication/Form1.cs
+++ MatrixMultiplication/Form1.cs
@@ -17,31 +17,24 @@ namespace MatrixMultiplication
             InitializeComponent();
         }
 
-        int[,] firstMatrix;
-        int[,] secondMatrix;
-        int[,] resultMatrix;
-        Label[,] firstMatrixLabels;
-        Label[,] secondMatrixLabels;
-        Label[,] resMatrixLabels;
-        int matrixSize;
-
         private void Button1_Click(object sender, EventArgs e)
         {
             panel1.Controls.Clear(); panel2.Controls.Clear(); panel3.Controls.Clear();
             Random rnd = new Random();
-            matrixSize = Convert.ToInt32(textBox1.Text);
+            int matrixSize = Convert.ToInt32(textBox1.Text);
             int n = matrixSize * 20 + 5 * (matrixSize - 1);
             panel1.Size = new Size(n, n); panel2.Size = new Size(n, n); panel3.Size = new Size(n, n);
             panel2.Location = new Point(n + 40, 40); panel3.Location = new Point(n * 2 + 60, 40);
             label2.Location = new Point(n + 20, 30 + n / 2); label2.Text = "X";
             label3.Location = new Point(n * 2 + 45, 30 + n / 2); label3.Text = "=";
-            firstMatrix = new int[matrixSize, matrixSize];
-            secondMatrix = new int[matrixSize, matrixSize];
-            resultMatrix = new int[matrixSize, matrixSize];
-            firstMatrixLabels = new Label[matrixSize, matrixSize];
-            secondMatrixLabels = new Label[matrixSize, matrixSize];
-            resMatrixLabels = new Label[matrixSize, matrixSize];
+            int[,] firstMatrix = new int[matrixSize, matrixSize];
+            int[,] secondMatrix = new int[matrixSize, matrixSize];
+            int[,] resultMatrix = new int[matrixSize, matrixSize];
+            Label[,] firstMatrixLabels = new Label[matrixSize, matrixSize];
+            Label[,]secondMatrixLabels = new Label[matrixSize, matrixSize];
+            Label[,] resMatrixLabels = new Label[matrixSize, matrixSize];
 
+            //Генерация матриц
             for (int i = 0; i < matrixSize; i++)
                 for (int j = 0; j < matrixSize; j++)
                 {
@@ -51,11 +44,7 @@ namespace MatrixMultiplication
                     firstMatrixLabels[i, j].Size = new Size(20, 20);
                     firstMatrixLabels[i, j].Location = new Point(j * 25, i * 25);
                     panel1.Controls.Add(firstMatrixLabels[i, j]);
-                }
 
-            for (int i = 0; i < matrixSize; i++)
-                for (int j = 0; j < matrixSize; j++)
-                {
                     secondMatrix[i, j] = rnd.Next(1, 4);
                     secondMatrixLabels[i, j] = new Label();
                     secondMatrixLabels[i, j].Text = secondMatrix[i, j].ToString();
@@ -63,47 +52,40 @@ namespace MatrixMultiplication
                     secondMatrixLabels[i, j].Location = new Point(j * 25, i * 25);
                     panel2.Controls.Add(secondMatrixLabels[i, j]);
 
-                }
-
-            List<Task> tasks = new List<Task>();
-            for (int i = 0; i < matrixSize; i++)            
-                for (int j = 0; j < matrixSize; j++)
-                {
-                    Task<int> task1 = new Task<int>(() => getResult(i, j));
-                    tasks.Add(new Task(delegate {
-                        getResult(i, j);
-                    }));
-                    
-                    //resultMatrix[i, j] = getResult(i, j);
-                    //resMatrixLabels[i, j] = new Label();
-                    //resMatrixLabels[i, j].Text = resultMatrix[i, j].ToString();
-                    //resMatrixLabels[i, j].Size = new Size(20, 20);
-                    //resMatrixLabels[i, j].Location = new Point(j * 25, i * 25);
-                    //panel3.Controls.Add(resMatrixLabels[i, j]);
-                }
-
-            foreach (var i in tasks)
+                    resMatrixLabels[i, j] = new Label();
+                    resMatrixLabels[i, j].Size = new Size(20, 20);
+                    resMatrixLabels[i, j].Location = new Point(j * 25, i * 25);
+                    panel3.Controls.Add(resMatrixLabels[i, j]);
+                }      
+            
+            //Создание массива тасков
+            Task[] tasks = new Task[matrixSize * matrixSize];
+            for (int t = 0; t < matrixSize * matrixSize; t++)
             {
-                i.Start();
+                var multiplicacion = new Task((param) =>
+                {
+                    int y = (int)param / matrixSize;
+                    int x = (int)param % matrixSize;
+                    resultMatrix[y, x] = 0;
+                    for (int i = 0; i < matrixSize; i++)
+                        resultMatrix[y, x] += firstMatrix[y, i] * secondMatrix[i, x];
+                }, t);
+                tasks[t] = multiplicacion;               
             }
-        }
+            
+            //Запуск всех тасков
+            foreach (var item in tasks)            
+                item.Start();
+            Task.WaitAll(tasks);
 
-        public void getResult(int x, int y)
-        {
-            int res = 0;
-            for (int i = 0; i < matrixSize; i++)           
-                res += firstMatrix[i, y] * secondMatrix[x, i];
-            resultMatrix[x, y] = res;
-            resMatrixLabels[x, y] = new Label();
-            resMatrixLabels[x, y].Text = res.ToString();
-            resMatrixLabels[x, y].Size = new Size(20, 20);
-            resMatrixLabels[x, y].Location = new Point(y * 25, x * 25);
-            panel3.Controls.Add(resMatrixLabels[x, y]);
-        }
-
-
+            //Отображение результатов
+            for (int i = 0; i < matrixSize; i++)
+                for (int j = 0; j < matrixSize; j++)
+                    resMatrixLabels[i, j].Text = resultMatrix[i, j].ToString();
+        }        
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
+            //Проверка на дурака
             button1.Enabled = (int.TryParse(textBox1.Text, out var x) ? true : false);
         }
     }
